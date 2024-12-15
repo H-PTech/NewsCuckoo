e17c766584c4ad6a1441da4da89f8df97b2b0c1c
KzeroJun <rladudwns164@gmail.com>
2024-12-15 19:20:01 +0900

    Init : 프로젝트 세팅

---
+++ src/main/java/com/hnptech/stocknewscuckoo/StocknewscuckooApplication.java
@@ -0,0 +1,13 @@
+package com.hnptech.stocknewscuckoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocknewscuckooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocknewscuckooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hnptech/stocknewscuckoo/StocknewscuckooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hnptech.stocknewscuckoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocknewscuckooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
